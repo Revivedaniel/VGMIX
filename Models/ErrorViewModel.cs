582fc47dbbaea8f23ce04c50944d3121deb74121
Revivedaniel <mlg.danielstark@gmail.com>
2022-05-19 21:24:32 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VGMIX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
